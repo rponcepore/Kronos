ad7e489125a643f0fc2cf58a54d71bc60377c26e
rponcepo <rponcepo@andrew.cmu.edu>
2025-02-15 16:00:59 -0500

    Starting to containerize.

--- frontend_kronos/vite.config.ts
+++ frontend_kronos/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Specify the development server port
+    port: 9000,
+  },
+  // Base name of your app
+  base: "/", 
 })
