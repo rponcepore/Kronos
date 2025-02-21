05ee7e92b424a521ae69c48ea57ff669b0a4805b
rponcepo <rponcepo@andrew.cmu.edu>
2025-02-21 15:13:37 -0500

    Updated dev_front_back.sh

--- scripts/dev_front_back.sh
+++ scripts/dev_front_back.sh
@@ -6,11 +6,48 @@
 # This script sets up just the docker container for the backend, exposing it on the correct ports.
 # The server is located at port 8000.
 
-# The frontend is located at port 9000, so there should be no collisions!
+# This script has been updated for new users running WSL.
+# check if rust is installed. 
+if ! command -v rustc &> /dev/null
+then
+    echo "Rust is not installed. Please install rust before running this script."
+    echo "You can install rust by running the following command:"
+    echo "curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh" #This works for linux and max
+    exit 1
+else
+  rustup update
+fi
+
+#check if make is installed
+if ! command -v make &> /dev/null
+then
+    echo "Make is not installed. Please install make before running this script."
+    echo "You can install make by running the following command:"
+    echo "sudo apt-get install make" #This works for linux. I don't know about mac. 
+    exit 1
+fi
+
+#Now, make sure that we are in the correct directory:
+
+EXPECTED_DIR="/home/$USER/Kronos/scripts"
+CURRENT_DIR="$(pwd)"
+
+if [ "$CURRENT_DIR" == "$EXPECTED_DIR" ]; then
+    echo "You are in the correct directory: $CURRENT_DIR"
+else
+    echo "Warning: You are in the wrong directory!"
+    echo "Current directory: $CURRENT_DIR"
+    echo "Expected directory: $EXPECTED_DIR"
+    echo ""
+    echo "Moving to correct directory."
+    cd $EXPECTED_DIR
+fi
 
 cd ..
 
+# The frontend is located at port 9000, so there should be no collisions!
 # make back can fail for lots of reasons. If it's something manageable, proceed.
+
 make back; retval=$?
 echo "retval = $retval"  ## now, adding more logging won't break the logic.
 case $retval in
@@ -34,6 +71,17 @@ esac
 cd backend_kronos
 cargo build
 echo "Target built. Building container."
+
+# We're going to try to run docker. First, check if the docker daemon is running.
+if ! docker info &> /dev/null
+then
+    echo "Docker is not running. Please start docker before running this script."
+    echo "If you are a Windows user, start docker desktop and re-run this script."
+    echo "For help, see: https://docs.docker.com/desktop/features/wsl/ "
+    exit 1
+fi
+
+
 
 # Build
 docker build --tag backend_kronos . 2>&1 
