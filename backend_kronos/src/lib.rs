62752bf42ba0191cb960a9694f4110f0291321a6
rponcepo <rponcepo@andrew.cmu.edu>
2025-03-14 12:48:30 -0400

    Refactored entities location. Successfully ran insertion and read tests. Tests will fail on second run at this time due to sideaffects.

--- backend_kronos/src/lib.rs
+++ backend_kronos/src/lib.rs
@@ -5,6 +5,7 @@
 pub mod routes; // points to our route folder
 pub mod configuration;
 pub mod startup;
+pub mod entities;
 
 
 
--- backend_kronos/tests/test_health_check.rs
+++ backend_kronos/tests/test_health_check.rs
@@ -3,8 +3,8 @@
 use std::net::TcpListener;
 use sea_orm::*;
 
-use backend_kronos::migration::entities::*;
 
+use backend_kronos::entities::{prelude::*, *};
 use backend_kronos::configuration::{self, get_configuration};
 
 const RANDOM_PORT: &str = "127.0.0.1:0";
@@ -36,47 +36,64 @@ async fn health_check_works () {
 
 #[tokio::test]
 async fn database_alive_test () {
-    let app_address = spawn_app();
+    // let app_address = spawn_app();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_string = configuration.database.connection_string();
     
     // Now attempt to connect to the database. 
-    match Database::connect(connection_string) {
+    match Database::connect(connection_string).await {
         Ok(..) => println!("Connection successful."),
-        Err(error) => panic!("Failed to connect to database."),
+        Err(e) => panic!("Failed to connect to database: {}", e),
     };
 }
 
 #[tokio::test]
 async fn database_insert_read_test () {
-    let app_address = spawn_app();
+    // let app_address = spawn_app();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_string = configuration.database.connection_string();
     
     // Now attempt to connect to the database. 
-    let connection = Database::connect(connection_string).expect("Failed to connect to database.");
+    let connection = Database::connect(connection_string).await.expect("Failed to connect to database.");
 
     // We'll insert and retrieve a test record, then delete the record.
     let test_record = test_table::ActiveModel {
         id: ActiveValue::Set(0),
-        name: ActiveValue::Set("Test_Name".to_owned()), //to_owned converts &str to String
+        title: ActiveValue::Set("Test_Name".to_owned()), //to_owned converts &str to String
         text: ActiveValue::Set("Test_Text".to_owned()),
     };
-    let res = TestTable::insert(test_record).exec(connection).await?;
+
+    match TestTable::insert(test_record).exec(&connection).await {
+        Ok(_) => {},
+        Err(e) => panic!("{}", e), 
+    };
 
     // Check for one and only one insertion into test_table
-    let test_records: Vec<test_table::Model> = TestTable::find().all(connection).await?;
+    let test_records : Vec<test_table::Model> = match TestTable::find().all(&connection).await {
+        Ok(test_records) => test_records,
+        Err(e) => panic!("{}", e),
+    };
+    
     assert_eq!(test_records.len(), 1);
 
     // Find by the id
-    let test_record_single : Option<test_table::Model> = TestTable::find_by_id(0).one(connection).await?;
+    let test_record_single : Option<test_table::Model> = match TestTable::find_by_id(0).one(&connection).await {
+        Ok(test_record_single) => test_record_single,
+        Err(e) => panic!("{}", e),
+    };
+
     assert_eq!(test_record_single.unwrap().id, 0);
 
     // Find the record by filter matching
-    let test_record_filter_one : Option<test_table::Model> = TestTable::find()
-        .filter(test_Table::Column::Text.eq("Test_Text"))
-        .one(connection)
-        .await?;
+    let test_record_filter_one : Option<test_table::Model> = match TestTable::find()
+        .filter(test_table::Column::Text.eq("Test_Text"))
+        .one(&connection)
+        .await {
+            Ok(test_record_filter_one) => test_record_filter_one,
+            Err(e) => panic!("{}", e),
+        };
+
+
     assert_eq!(test_record_filter_one.unwrap().id, 0);
 
 }
@@ -87,9 +104,9 @@ async fn database_insert_read_test () {
 /* Spawn application in the background as a helper function */
 fn spawn_app() -> String {
     let listener = TcpListener::bind(RANDOM_PORT)
-    .expect("Failed to bind random port");
+        .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = backend_kronos::run_server(listener).expect("Failed to bind address");
+    let server = backend_kronos::startup::run_server(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
