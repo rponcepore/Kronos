bfcd7c8ed63c16e85478634cfbc50768a13a3d9b
Olivia Beattie <obeattie@andrew.cmu.edu>
2025-03-17 14:57:32 -0400

    Remove unused image-1.png and update BackgroundOverlay component to eliminate background image

--- frontend_kronos/src/components/BackgroundOverlay.tsx
+++ frontend_kronos/src/components/BackgroundOverlay.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import backgroundImage from "/src/assets/image-1.png";
 import overlayImage from "/src/assets/rectangle-1.svg";
 import "../styles/BackgroundOverlay.css";
 
 const BackgroundOverlay = () => {
   return (
     <div className="background-container">
-      <img className="background" alt="Background" src={backgroundImage} />
       <img className="overlay" alt="Overlay" src={overlayImage} />
     </div>
   );
