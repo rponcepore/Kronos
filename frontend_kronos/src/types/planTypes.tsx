078e8300e63cff34318c70d5d29694e27ac7a5fe
rponcepo <rponcepo@andrew.cmu.edu>
2025-03-25 09:59:05 -0400

    Provided new interface for plan type.

--- frontend_kronos/src/types/planTypes.tsx
+++ frontend_kronos/src/types/planTypes.tsx
@@ -9,3 +9,19 @@ export interface Plan {
     taskedTo: string;
     mission: string;
   }
+
+/*
+
+export type Plan = {
+
+    id: number,
+    unit: String,
+    parent_plan: number, // can be null,
+    fiscal_year: number,
+    serial_number: number,
+    classification: string,
+    name: string,
+
+}
+
+*/
