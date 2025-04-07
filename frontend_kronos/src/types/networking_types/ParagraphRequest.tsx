21b51f8440bf3c77996ff51e3cec832e59e23edd
rponcepo <rponcepo@andrew.cmu.edu>
2025-04-07 09:57:09 -0400

    Refined ParagraphRequest type.

--- frontend_kronos/src/types/networking_types/ParagraphRequest.tsx
+++ frontend_kronos/src/types/networking_types/ParagraphRequest.tsx
@@ -1,6 +1,9 @@
 //! ParagraphRequest.tsx
 
 export type ParagraphRequest = {
-    paragraph_id: number | null,
-    parent_paragraph_id: number | null,
+    paragraph_id: number | null, // This is the target paragraph
+    // parent_paragraph_id: number | null, 
+    insert_method: "ABOVE" | "BELOW" | "SUBPARAGRAPH",
+    new_title: string,
+    new_text: string,
 }
