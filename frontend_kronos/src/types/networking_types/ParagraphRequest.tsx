f271ab5455f5b75d7e2f82256a725b0bab2a4272
rponcepo <rponcepo@andrew.cmu.edu>
2025-04-07 11:42:49 -0400

    Wrote new test.

--- frontend_kronos/src/types/networking_types/ParagraphRequest.tsx
+++ frontend_kronos/src/types/networking_types/ParagraphRequest.tsx
@@ -3,7 +3,7 @@
 export type ParagraphRequest = {
     paragraph_id: number | null, // This is the target paragraph
     // parent_paragraph_id: number | null, 
-    insert_method: "ABOVE" | "BELOW" | "SUBPARAGRAPH",
-    new_title: string,
-    new_text: string,
+    insert_method: "ABOVE" | "BELOW" | "SUBPARAGRAPH" | null,
+    new_title: string | null,
+    new_text: string | null,
 }
--- frontend_kronos/vitest.config.ts
+++ frontend_kronos/vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ["src/tests/**/*.test.tsx"], // Ensures tests outside `src/` are included
+    include: ["src/tests/**/*.test.tsx"], 
   },
   resolve: {
     alias: {
